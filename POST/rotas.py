17863b9a7ce6a169dc79a86eef97f6274ee0b688
MarcoAntonioGritti <m9946658@gmail.com>
2025-01-13 12:37:41 -0300

    Reparando métodos

--- POST/rotas.py
+++ POST/rotas.py
@@ -1,19 +1,28 @@
 from POST.comandos import adicionar_produto
-from flask import Blueprint,jsonify,request
+from flask import Blueprint, jsonify, request
 
-rotas_post = Blueprint('rotas_post',__name__)
+rotas_post = Blueprint("rotas_post", __name__)
 
-@rotas_post.route('/produto',methods=['POST'])
+
+@rotas_post.route("/produto", methods=["POST"])
 def post_produto():
     dados = request.get_json()
     lista_resposta = []
 
-    nome = dados.get('nome')
-    descricao = dados.get('descricao')
-    preco = dados.get('preco')
-    quantidade = dados.get('quantidade')
+    for item in dados:
+        nome = item.get("nome")
+        descricao = item.get("descricao")
+        preco = item.get("preco")
+        quantidade = item.get("quantidade")
 
-    lista_resposta.append({'nome': nome, 'descricao':descricao, 'preco':preco,' quantidade':quantidade})
-    adicionar_produto(nome, descricao, preco, quantidade)
+        lista_resposta.append(
+            {
+                "nome": nome,
+                "descricao": descricao,
+                "preco": preco,
+                " quantidade": quantidade,
+            }
+        )
+        adicionar_produto(nome, descricao, preco, quantidade)
 
     return jsonify(lista_resposta), 201
